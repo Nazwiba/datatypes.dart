b3e743cd8ba6ce9bd6b5da386f29e7025f0ff7d6
Nasweeba A <nasweebaa68@gmail.com>
2023-09-25 12:20:19 +0530

    commit

---
+++ bin/iteration or loops/for_loop.dart
@@ -0,0 +1,26 @@
+void main() {
+  ///syntax : for(initialization ; condition checking ; increment or decrement operator){ }
+  for (int i = 1; i <= 10; i++) {
+    /// i++ is postfix operator
+    print(1);
+  }
+  for (int i = 2; i <= 10; i++) {
+    print(2);
+  }
+  for (int i = 3; i <= 10; i++) {
+    print(3);
+  }
+  for (int i = 4; i <= 10; i++) {
+    print(4);
+  }
+  for (int i = 11; i <= 10; i++) {
+    print(11);
+  }
+}
+/// i=1 i<=10 true print 1 i++ 2
+/// i =2 i<=10 true print 2 i++ 3
+/// i = 3 i<= 10 true print 3 i++ 4
+/// i = 4 i<=10 true print 4 i++ 5
+/// ...........................
+/// i = 10 i<=10 true print 10 i++ 11
+/// i = 11 i<=10 false exit from for loop
---
+++ bin/iteration or loops/while_loop.dart
@@ -0,0 +1,16 @@
+void main() {
+  /// syntax :
+  /* initialization;
+        while(condition checking){
+              statements to be executed ;
+              increment/decrement counter;
+  }
+  */
+
+  int i = 1;
+  while (i <= 10) {
+    // i++; we can write incr/decre counter here or below
+    print(1);
+    i++;
+  }
+}
