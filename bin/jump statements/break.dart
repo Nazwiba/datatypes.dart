31000f38f9ea37cd05069d4bbad2393f658ce7e6
Nasweeba A <nasweebaa68@gmail.com>
2023-09-26 11:47:40 +0530

    commit

---
+++ bin/jump statements/break.dart
@@ -0,0 +1,9 @@
+void main() {
+  ///break , continue , return
+  for (int i = 1; i <= 100; i++) {
+    if (i == 51) { /// print upto 50 natural numbers excluded 51 => i==51 ,== means not equalto
+      break;
+    }
+    print(i);
+  }
+}
---
+++ bin/jump statements/continue.dart
@@ -0,0 +1,8 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 2 != 0) {
+      continue; /// to skip particular iteration
+    }
+    print(i);
+  }
+}
