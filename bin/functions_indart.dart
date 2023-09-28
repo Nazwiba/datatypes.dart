303d433a2cfc61f0db8dcb811cbce5158d343b02
Nasweeba A <nasweebaa68@gmail.com>
2023-09-28 12:50:47 +0530

    commit

---
+++ bin/functions_indart.dart
@@ -0,0 +1,55 @@
+void main() {
+  func1();
+  // print(func2());
+  String n = func2();
+  print(n); //the data that returned from func2 stored in 'n'
+  func3(100, 30); // here 100 and 30 are the actual parameters
+  print(func4(10, 'GOOD', 9));
+  func5();
+  func6(2, 4);
+  print(func7());
+  print(func8(1000));
+}
+
+/// User defined default function without return type
+void func1() {
+  // func1()in this bracket nothing is there it is a default function, if something is there it is parameter
+  print("default function func1");
+}
+
+/// User default function with return type(rethurn type can be int, String,double,bool,list,etc)
+String func2() {
+  /// it is  not be  null
+  String data = "hai";
+  int a = 100;
+  print(a);
+  return data; // we can return only one value, type of value be similar to the return type of function
+}
+
+/// User defined parameterised function without return type
+/// here a and b are parameters or arguments or formal parameters(parameters at the creation of function)
+void func3(int a, int b) {
+  // parameters can be int,String,double,bool,list etc
+
+  int sum = a + b; // here sum is a local variable
+  print('sum =$sum');
+}
+
+// user defined parameterised function with return type
+int func4(int a, String b, double c) {
+  print('value of a = $a b = $b c = $c');
+  return 0;
+}
+
+/// lamda function - function that contains only one statement to be executed
+/// lambda function without return type and parameter
+void func5() => print("hello"); // denoted as => instead of {}
+
+///lambda function without return type and with parameter
+void func6(int a, int b) => print("sum = ${a + b}");
+
+///lambda function with return type and without parameter
+int func7() => 10;
+
+/// lambda function with return type and with parameter
+int func8(int x) => x + 7;
---
+++ bin/program/userdefined_parme_without_return.dart
@@ -0,0 +1,8 @@
+void main() {
+  func1("Anu", 22, 9.5, "anu123@gmail.com", 7025194906);
+  func1("Nazwi", 23, 9.0, "nazwi123@gmail.com", 9539914795);
+}
+
+void func1(String name, int age, double mark, String email, int phone) {
+  print("name = $name, age = $age, mark = $mark, email = $email, phone = $phone"); // \n is used to get line by line ouput
+}
